7c5418069498727c22ea21cb96c7826eb2b9ee12
Kaushal <90102364+Itsmekaushal@users.noreply.github.com>
2025-04-03 10:56:53 +0530

    Update socket.service.ts

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -9,11 +9,27 @@ export class SocketService {
     this.socket = io('http://localhost:5000');
   }
 
+  joinRoom(username: string, roomID: string, accessID: string) {
+    this.socket.emit('joinRoom', { username, roomID, accessID });
+  }
+
   sendMessage(message: { sender: string; text: string }) {
     this.socket.emit('message', message);
   }
 
   onMessage(callback: (message: any) => void) {
     this.socket.on('message', callback);
+  }
+
+  onUserCount(callback: (count: number) => void) {
+    this.socket.on('userCount', callback);
+  }
+
+  onError(callback: (error: string) => void) {
+    this.socket.on('error', callback);
+  }
+
+  disconnect() {
+    this.socket.disconnect();
   }
 }
